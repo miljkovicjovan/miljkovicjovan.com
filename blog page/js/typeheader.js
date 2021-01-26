f1304c57b43ad287ad24545839e9766b802ab51c
Jovan M <77690201+miljkovicjovan@users.noreply.github.com>
2021-01-26 15:06:38 +0100

    Add files via upload

---
+++ blog page/js/typeheader.js
@@ -0,0 +1,5 @@
+var typed = new Typed('.header-typed', {
+    strings: ["Hey, I'm Jovan.", "Hey, I'm a Software Engineer", "Hey, I'm a Web Developer", "Hey, I'm a student", "Hey, I'm Jovan."],
+    typeSpeed: 90 ,
+    backSpeed: 40
+});
